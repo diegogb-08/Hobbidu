23064526c7bbe8cf70516f810e7cf8ffd6ffc515
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-07 12:22:07 +0200

    feat (db): setup prisma

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,22 @@
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import type { Users } from "@prisma/client";
+import { db } from "../utils/db.server";
+
+type LoaderData = { users: Array<Users> };
+
+export const loader: LoaderFunction = async () => {
+  const data: LoaderData = {
+    users: await db.users.findMany(),
+  };
+  return json(data);
+};
+
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>()
+  console.log(data)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Welcome to Remix</h1>
