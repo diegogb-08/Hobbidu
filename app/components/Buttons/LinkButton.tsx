140db59748b9145481b00ad94313220cf7ee7493
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-15 11:16:40 +0200

    refactor LoginButton for a generic LinkButton

---
+++ app/components/Buttons/LinkButton.tsx
@@ -0,0 +1,20 @@
+import type { FC, RefAttributes } from 'react';
+import type { RemixLinkProps } from '@remix-run/react/dist/components';
+import { Link } from '@remix-run/react';
+
+interface Props extends RemixLinkProps, RefAttributes<HTMLAnchorElement> {
+  text: string;
+}
+
+const LinkButton: FC<Props> = ({ text, className, ...props }) => {
+  return (
+    <Link
+      {...props}
+      className={`bg-transparent hover:bg-primary text-label font-semibold hover:text-fontcolor-white py-2 px-4 ml-4 border hover:border-transparent rounded ${className}`}
+    >
+      {text}
+    </Link>
+  );
+};
+
+export default LinkButton;
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,6 +1,6 @@
 import useAuth from '~/hooks/useAuth';
 import logo from '../../public/img/logo.png';
-import LoginButton from './Buttons/LoginButton';
+import LinkButton from './Buttons/LinkButton';
 import NavBar from './NavBar';
 
 const Header = () => {
@@ -9,7 +9,14 @@ const Header = () => {
     <div className="box-border h-16 flex-1 flex justify-center items-center bg-white border-b-[0.5px] border-b-gray">
       <img className="h-4/5" src={logo} alt="logo" />
       <div className="absolute right-4">
-        {isAuthenticated ? <NavBar /> : <LoginButton />}
+        {isAuthenticated ? (
+          <NavBar />
+        ) : (
+          <>
+            <LinkButton to="account/register" text="Register" />
+            <LinkButton to="account/login" text="Login" />
+          </>
+        )}
       </div>
     </div>
   );
