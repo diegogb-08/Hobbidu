6170b4d2084dde80ee2ad42369d51be74a132376
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-15 10:44:49 +0200

    add account navigation

--- app/routes/account.tsx
+++ app/routes/account.tsx
@@ -1,6 +1,9 @@
-import { Outlet } from '@remix-run/react';
+import { Outlet, useMatches } from '@remix-run/react';
+import { Link } from 'react-router-dom';
 
 const Account = () => {
+  const matches = useMatches();
+
   return (
     <div className="w-full h-[calc(100vh-4rem)] flex justify-center items-center">
       <div className="h-full flex-1 bg-login-background bg-center bg-no-repeat bg-cover" />
@@ -8,8 +11,22 @@ const Account = () => {
         <div className="h-5/6 w-full bg-white p-10 rounded border-[0.5px] border-gray flex flex-col mb-4">
           <Outlet />
         </div>
-        <div className="h-1/6 items-stretch w-full bg-white p-10 rounded border-[0.5px] border-gray flex-col">
-          Something goes here
+        <div className="h-1/6 w-full bg-white p-10 rounded border-[0.5px] border-gray flex justify-center items-center">
+          {matches[2].pathname.includes('login') ? (
+            <>
+              <p>Don't have an account?</p>
+              <Link className="ml-4 font-bold text-fontcolor2" to="register">
+                Register
+              </Link>
+            </>
+          ) : (
+            <>
+              <p>Do you have an account?</p>
+              <Link className="ml-4 font-bold text-fontcolor2" to="login">
+                Sign In
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
