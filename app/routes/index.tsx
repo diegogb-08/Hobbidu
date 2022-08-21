78e0a165cc37029e982e3e88ea8c3018425e29c8
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-22 01:40:54 +0200

    refactor index console

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,17 @@
 import type { LoaderFunction } from '@remix-run/node'
 import { useLoaderData } from '@remix-run/react'
 import AppContainer from '~/components/Layouts/AppContainer'
+import type { ErrorAuth, UserAuth } from '~/services/auth.server'
 import { authenticator } from '~/services/auth.server'
 
-export const loader: LoaderFunction = async ({ request }) => {
+export const loader: LoaderFunction = async ({ request }): Promise<UserAuth | ErrorAuth | null> => {
   return await authenticator.isAuthenticated(request)
 }
 
 export default function Index() {
-  const indexData = useLoaderData<typeof loader>()
-  console.log({ indexData })
+  const data = useLoaderData<typeof loader>()
   return (
-    <AppContainer user={indexData?.user}>
+    <AppContainer user={data?.user}>
       <div className='h-full w-full bg-secondary'>hello Index</div>
     </AppContainer>
   )
