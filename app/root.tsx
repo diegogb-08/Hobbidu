16a658d15733c3def120dcf2047ab9f11c3d4012
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-24 22:36:20 +0200

    add favicon and change title

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,7 @@ export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles, as
 export const meta: MetaFunction = () => ({
   // eslint-disable-next-line unicorn/text-encoding-identifier-case
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Hobbidu',
   viewport: 'width=device-width,initial-scale=1'
 })
 
