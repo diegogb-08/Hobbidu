ae78ac78754e64a276f43fddc06aabe094f3aa93
DiegoG <diego.garciabrisa@maibornwolff.de>
2022-08-25 18:21:29 +0200

    add nav icons

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,11 +1,20 @@
 import { Link } from '@remix-run/react'
+import AvatarIcon from '~/icons/AvatarIcon'
+import CalendarPlusIcon from '~/icons/CalendarPlusIcon'
+import HomeIcon from '~/icons/HomeIcon'
 
 const NavBar = () => {
   return (
     <div className='flex w-80 justify-between'>
-      <Link to='/'>Home</Link>
-      <Link to='/create-event'>Create Event</Link>
-      <Link to='/profile'>Profile</Link>
+      <Link to='/'>
+        <HomeIcon color='#acacacc4' />
+      </Link>
+      <Link to='/create-event'>
+        <CalendarPlusIcon color='#acacacc4' />
+      </Link>
+      <Link to='/profile'>
+        <AvatarIcon color='#acacacc4' />
+      </Link>
     </div>
   )
 }
