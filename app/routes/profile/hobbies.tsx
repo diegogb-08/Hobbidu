66935d21868f98943b40eaecd8942e9a3183595e
DiegoG <diego.garciabrisa@maibornwolff.de>
2023-02-24 09:27:59 +0100

    fix hobbies page WIP

--- app/routes/profile/hobbies.tsx
+++ app/routes/profile/hobbies.tsx
@@ -1,112 +1,78 @@
 import { Chip } from '@mui/material'
 import type { Hobby } from '@prisma/client'
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 
-import type { ActionFunction, LoaderFunction } from '@remix-run/node'
+import type { DataFunctionArgs } from '@remix-run/node'
 import { json } from '@remix-run/node'
-import { Form, useActionData, useLoaderData, useMatches, useTransition } from '@remix-run/react'
-import { useCallback, useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { useLoaderData, useTransition } from '@remix-run/react'
+import { withZod } from '@remix-validated-form/with-zod'
+import { useState } from 'react'
+import { ValidatedForm } from 'remix-validated-form'
+import { z } from 'zod'
 import TextField from '~/components/Form/TextField'
 import { authenticator } from '~/services/auth.server'
-import { getSession } from '~/services/session.server'
-import type { UserAuth } from '~/types/types'
 import { db } from '~/utils/db.server'
 import SubmitButton from '../../components/Buttons/SubmitButton'
 
-interface ActionData {
-  success?: boolean
-  hobbyId?: string
-  hobby?: FormDataEntryValue | null
-  errors?: string
-  message?: string
-}
-export const action: ActionFunction = async ({ request }): Promise<ActionData> => {
-  const session = await getSession(request.headers.get('Cookie'))
-  const userAuth = (await session.get('sessionKey')) as UserAuth | undefined
+const HobbiesSchema = z.object({
+  hobbyName: z.string().optional(),
+  hobbyIds: z.array(z.string()).optional()
+})
 
-  const formData = await request.formData()
-  const hobby = formData.get('hobby')
-  const hobbyName = (hobby as string)?.toUpperCase()
-  if (userAuth?.user?.id) {
-    if (hobby) {
-      try {
-        const newHobby = await db.hobby.create({
-          data: {
-            name: hobbyName,
-            user_id: userAuth?.user?.id
-          }
-        })
-        return { success: true, hobbyId: newHobby.id }
-      } catch (error) {
-        if (error instanceof PrismaClientKnownRequestError) {
-          console.error(error)
-          const existingHobby = await db.hobby.findUnique({
-            where: {
-              name: hobbyName
-            }
-          })
-          return {
-            success: true,
-            hobby,
-            hobbyId: existingHobby?.id,
-            errors: `"${hobbyName}" already exist. It has been selected for you.`
-          }
-        }
-        console.error(error)
-        return { hobby, errors: 'Something went wrong' }
-      }
-    }
-    return { hobby, errors: 'Field cannot be empty' }
-  }
-  return { message: 'Action not authorized.' }
-}
+const validator = withZod(HobbiesSchema)
 
-export const loader: LoaderFunction = async ({ request }) => {
-  await authenticator.isAuthenticated(request, {
+export const loader = async ({ request }: DataFunctionArgs) => {
+  const clonedRequest = new Request(request)
+  const userAuth = await authenticator.isAuthenticated(request, {
     failureRedirect: '/account/login'
   })
-
   const hobbies = await db.hobby.findMany()
+  const result = await validator.validate(clonedRequest)
+  const hobbyIds: string[] = result.data?.hobbyIds || []
+  const hobbyName = result.data?.hobbyName
+  if (hobbyName) {
+    const hobbyNameUpperCase = hobbyName.toUpperCase()
+    const foundHobby = hobbies.find((_) => _.name === hobbyNameUpperCase)
+    if (foundHobby) {
+      hobbyIds.push(foundHobby?.id)
+    } else {
+      const newHobby = await db.hobby.create({
+        data: {
+          name: hobbyNameUpperCase,
+          user_id: userAuth?.user?.id
+        }
+      })
+      hobbyIds.push(newHobby?.id)
+    }
+  }
 
-  const hobbiesRecord = hobbies.reduce((previousHobby, currentHobby) => {
+  const hobbiesRecord: Record<string, Hobby> = hobbies.reduce((previousHobby, currentHobby) => {
     return {
       ...previousHobby,
       [currentHobby.id]: currentHobby
     }
   }, {})
-  return json({ hobbies: hobbiesRecord })
+  return json({ hobbies: hobbiesRecord, hobbyIds, hobbyName })
 }
 
 const Hobbies = () => {
-  const actionData = useActionData<ActionData>()
   const transition = useTransition()
-  const match = useMatches()
-  const { hobbies } = useLoaderData<{ hobbies: Record<string, Hobby> }>()
-  const [hobbyIds, setHobbyIds] = useState<string[]>(match[0].data?.user?.hobbies)
+  const { hobbies, hobbyIds } = useLoaderData<typeof loader>()
+  const [selectedHobbyIds, setSelectedHobbyIds] = useState<string[]>([...hobbyIds])
   const isSubmitting = transition.state === 'submitting'
 
-  const handleSelectHobby = useCallback(
-    (hobbyId: string) => {
-      let newHobbyIds = [...hobbyIds]
-      if (hobbyIds.includes(hobbyId)) {
-        newHobbyIds = hobbyIds.filter((id) => id !== hobbyId)
-      } else {
-        newHobbyIds.push(hobbyId)
-      }
-      setHobbyIds(newHobbyIds)
-    },
-    [hobbyIds]
-  )
-
-  useEffect(() => {
-    if (actionData?.success && actionData?.hobbyId) {
-      handleSelectHobby(actionData?.hobbyId)
+  const handleSelectHobby = (hobbyId: string) => {
+    let newHobbyIds = [...selectedHobbyIds]
+    if (selectedHobbyIds.includes(hobbyId)) {
+      newHobbyIds = selectedHobbyIds.filter((id) => id !== hobbyId)
+    } else {
+      newHobbyIds.push(hobbyId)
     }
-  }, [actionData, hobbies, handleSelectHobby])
+    setSelectedHobbyIds(newHobbyIds)
+  }
+  console.log(selectedHobbyIds)
 
   return (
-    <>
+    <ValidatedForm validator={validator} method='get'>
       <h2 className='text-xl font-bold pb-6'>Please select the hobbies you want to follow!</h2>
       <div className='flex flex-wrap justify-center'>
         {Object.values(hobbies).map((hobby) => {
@@ -123,29 +89,29 @@ const Hobbies = () => {
         })}
       </div>
       <h3 className='text-lg font-bold pt-6 pb-6'>You can't find your hobby? Add it yourself below</h3>
-      <Form method='post' className='flex w-full justify-center'>
-        <TextField name='hobby' type='text' placeholder='Add your missing hobby here. e.g. CLIMBING' />
+      <div className='flex w-full justify-center'>
+        <TextField name='hobbyName' type='text' placeholder='Add your missing hobby here. e.g. CLIMBING' />
         <button
           type='submit'
           className='ml-4 h-[38px] w-40 p-4 text-center flex justify-center items-center bg-transparent font-semibold hover:text-fontcolor-white py-2 px-4 border hover:border-transparent rounded hover:bg-primary text-label'
         >
           {isSubmitting ? 'Adding...' : 'Add Hobby'}
         </button>
-      </Form>
-      {actionData?.message && (
+      </div>
+      {/* {actionData?.message && (
         <h3 className='text-red font-bold text-xl'>
           {actionData?.message} Please{' '}
           <Link className='underline' to='/account/login'>
             Login
           </Link>
         </h3>
-      )}
+      )} */}
 
-      {hobbyIds.length > 0 && (
+      {selectedHobbyIds.length > 0 && (
         <div>
           <h3 className='text-lg font-bold pt-6 pb-6'>Selected Hobbies:</h3>
           <div className='flex flex-wrap justify-center'>
-            {hobbyIds.map((hobbyId) => {
+            {selectedHobbyIds.map((hobbyId) => {
               const hobby = hobbies[hobbyId]
               return (
                 <Chip
@@ -159,19 +125,15 @@ const Hobbies = () => {
               )
             })}
           </div>
-          <Form method='post' action='/'>
-            <h3 className='text-lg font-bold pt-6 pb-6'>
-              Press continue once you select the hobbies you want to follow
-            </h3>
-            <div className='w-full flex justify-center'>
-              <div className='w-40'>
-                <SubmitButton name='hobbyIds' value={hobbyIds} text='Continue' />
-              </div>
+          <h3 className='text-lg font-bold pt-6 pb-6'>Press continue once you select the hobbies you want to follow</h3>
+          <div className='w-full flex justify-center'>
+            <div className='w-40'>
+              <SubmitButton name='hobbyIds' value={selectedHobbyIds} text='Continue' />
             </div>
-          </Form>
+          </div>
         </div>
       )}
-    </>
+    </ValidatedForm>
   )
 }
 
